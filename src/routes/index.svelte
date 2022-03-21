949edf23ef6a5f54dbe6ccb7e0bec811d95c6f8a
sharathdt <sharath.dt@gmail.com>
2022-03-21 16:37:57 +0530

    init

---
+++ src/routes/index.svelte
@@ -0,0 +1,34 @@
+<script context="module">
+  export const load = async ({ fetch }) => {
+    const res = await fetch("/graphql", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: `query { 
+        books {
+          title
+          author
+        }
+      }`,
+      }),
+    });
+
+    const { data } = await res.json();
+
+    return {
+      props: { data },
+    };
+  };
+</script>
+
+<script>
+  export let data;
+</script>
+
+{JSON.stringify(data)}
+<h1>Welcome to SvelteKit</h1>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
